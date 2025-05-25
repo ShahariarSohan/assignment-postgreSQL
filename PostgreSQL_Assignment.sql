0e074a7d258688a3d2aa7deae548f121c9db0ad8
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-05-25 11:40:31 +0600

    some solution added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,15 +1,17 @@
 -- Active: 1747449241893@@localhost@5432@conservation_db
 -- create a database
 CREATE DATABASE conservation_db;
--- import -------> I have used backticks(``) for all tables and columns name.
---create table `rangers`
+
+--create table rangers
+
 CREATE TABLE rangers(
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     region TEXT
 );
 
--- create table `species`
+-- create table species
+
 CREATE TABLE species(
    species_id SERIAL PRIMARY KEY,
    common_name VARCHAR(255),
@@ -17,7 +19,8 @@ CREATE TABLE species(
    discovery_date DATE,
    conservation_status VARCHAR(255)
 );
--- create table `sightings`
+-- create table sightings
+
 CREATE TABLE sightings(
     sighting_id SERIAL PRIMARY KEY,
     ranger_id INT REFERENCES rangers(ranger_id),
@@ -28,6 +31,7 @@ CREATE TABLE sightings(
 );
 
 -- insert values 
+
 INSERT INTO rangers (name,region)
 VALUES ('Alice Green','Northern Hills'),
        ('Bob White','River Delta'),
@@ -42,12 +46,55 @@ INSERT INTO sightings(ranger_id,species_id,location,sighting_time,notes)
 VALUES( 1 , 1,  'Peak Ridge', '2024-05-10 07:45:00 ', 'Camera trap image captured'),
        (2 , 2,  ' Bankwood Area','2024-05-12 16:20:00', 'Juvenile seen'),
        (3 , 3 ,  'Bamboo Grove East' ,' 2024-05-15 09:10:00', 'Feeding observed' ),
-       (1 , 2 ,  ' Snowfall Pass', '2024-05-18 18:30:00 ',NULL );
+       (2 , 1 ,  ' Snowfall Pass', '2024-05-18 18:30:00 ',NULL );
 
 select * from  rangers;      
 select * from  species;      
 select * from  sightings;
+
 -----> PROBLEM NO 1 <--------
+
 INSERT INTO rangers (name,region)
 VALUES ('Derek Fox','Coastal Plains');
+
 ----> PROBLEM NO 2 <-------
+
+SELECT  COUNT(DISTINCT species_id) AS  unique_species_count FROM sightings;
+
+----> PROBLEM NO 3 <-------
+
+SELECT * FROM sightings WHERE location LIKE '%Pass%';
+
+----> PROBLEM NO 4 <-------
+
+SELECT name,COUNT(*) AS total_sightings FROM rangers
+INNER JOIN sightings USING (ranger_id) GROUP BY ranger_id;
+
+----> PROBLEM NO 5<-------
+
+SELECT common_name FROM species
+FULL JOIN sightings USING(species_id) WHERE sighting_id IS NULL;
+
+----> PROBLEM NO 6<-------
+
+SELECT common_name,sighting_time,name FROM rangers 
+JOIN sightings USING (ranger_id)
+JOIN species USING (species_id)
+ORDER  BY sighting_time DESC
+LIMIT 2;
+
+----> PROBLEM NO 7<-------
+
+UPDATE species
+SET conservation_status='Historic'
+WHERE EXTRACT(YEAR FROM discovery_date) < 1800;
+
+----> PROBLEM NO 8<-------
+
+SELECT * FROM sightings;
+
+----> PROBLEM NO 9<-------
+
+DELETE  FROM rangers
+FULL JOIN sightings USING (ranger_id)
+WHERE sighting_id IS NULL;
